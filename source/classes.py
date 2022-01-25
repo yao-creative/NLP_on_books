a7ac2da941b76bf6f51c457d1c0535fc6a199b38
Yi Yao Tan <yytanwork@gmail.com>
2022-01-25 21:59:30 +0100

    Added book and library class to store books as pdf

---
+++ source/classes.py
@@ -0,0 +1,65 @@
+import PyPDF4
+import pickle
+import threading
+import logging
+
+
+class Library(): #collection of books
+    
+    #one day will automate "fill library" where it will fill it self with web scrapped books for processing
+    
+    def __init__(self):
+        self.books = dict()
+        
+        
+    def add_book(self,book):
+        """Add book to the library dictionary"""
+        if (book.title,book.path) not in self.books: #store the books in the dictionary
+            self.books[(book.title,book.path)] = book
+            
+            
+            
+class Book(): #book contains processable texts
+    
+    def __init__(self, title, path):
+        """title = title of the book save file (better to have underscores), path is path to book pdf"""
+        
+        try:
+            if path[-3:] != "pdf":
+                print(f"title: {title} was not a pdf file")
+                return -1
+            self.title = title
+            self.path = path
+            self.text = dict() # create a dictionary of text where each key is page and value is text on a page
+        except:
+            return -1
+        
+    def retrieve_text(self, auto_save=True):
+        """Given the book with the title and path properties auto save set to true"""
+        
+        try:
+
+            pdfFileObj = open(self.path, 'rb')  #select file
+            pdfReader = PyPDF4.PdfFileReader(pdfFileObj,strict=False)
+            numberPages = pdfReader.numPages
+            
+            for i in range(0,numberPages): #get all of the pages and append to a book dictionary
+                pageObj = pdfReader.getPage(i)
+                self.text[i] = pageObj.extractText()
+                
+                
+            logging.info(f"Retrieved: \033[92m {self.title}")
+            logging.info(f"Number of Pages: \033[92m {pdfReader.numPages}")
+            
+            
+            if auto_save: #save the books
+                
+                with open(f"books_txt/{self.title}.var", "wb") as outfile1:
+                    pickle.dump(self.text, outfile1)
+                with open(f"books_var/{self.title}.txt", "w") as outfile2:
+                    outfile2.write("\n_____________________________________________\n".join(self.text.values()))
+                logging.info(f"File Saved, Title: \033[35m {self.title}")
+                
+                
+        except:
+            print(f"Could not find: {self.path}")
