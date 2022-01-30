1891493e230c3e8b82057289f716f12a6a14bdd6
elisabeth04 <liuyufei43@126.com>
2022-01-30 01:02:33 +0100

    Solving missing space problem in split sentence

--- process_book.py
+++ process_book.py
@@ -3,7 +3,7 @@ from nltk.tokenize import sent_tokenize
 
 def split_sentence(filename):
     with open(filename) as f:
-        lines = ''.join([line.strip().strip('*') for line in f])
+        lines = ' '.join([line.strip().strip('*') for line in f])
         f.close()
         return sent_tokenize(lines)
 
--- test_process_book.py
+++ test_process_book.py
@@ -6,6 +6,6 @@ import process_book
 for i in range(100):
         print(f.readline())
 with open("books_txt/A_Pail_of_Air_by_Fritz_Leiber.txt") as f:
-    lines = ''.join([line.strip() for line in f])'''
+    lines = ' '.join([line.strip() for line in f])'''
     
 print(process_book.split_sentence("books_txt/Youth_by_Isaac_Asimov.txt"))
