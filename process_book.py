45775ec2858d1dbe2a95b6a7fc8c70d20950dd56
elisabeth04 <liuyufei43@126.com>
2022-01-30 01:38:56 +0100

    adding entity and relation models

--- process_book.py
+++ process_book.py
@@ -1,5 +1,9 @@
-import nltk
 from nltk.tokenize import sent_tokenize
+
+import numpy as np
+import logging 
+import json
+import argparse
 
 def split_sentence(filename):
     with open(filename) as f:
@@ -7,3 +11,83 @@ def split_sentence(filename):
         f.close()
         return sent_tokenize(lines)
 
+"""Borrowed code - subject to further study"""
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
+task_ner_labels = {
+    'ace04': ['FAC', 'WEA', 'LOC', 'VEH', 'GPE', 'ORG', 'PER'],
+    'ace05': ['FAC', 'WEA', 'LOC', 'VEH', 'GPE', 'ORG', 'PER'],
+    'scierc': ['Method', 'OtherScientificTerm', 'Task', 'Generic', 'Material', 'Metric'],
+}
+
+def get_labelmap(label_list):
+    label2id = {}
+    id2label = {}
+    for i, label in enumerate(label_list):
+        label2id[label] = i + 1
+        id2label[i + 1] = label
+    return label2id, id2label
+
+logger = logging.getLogger('root')
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+ner_label2id, ner_id2label = get_labelmap(task_ner_labels[args.task])
+def save_model(model, args):
+    """
+    Save the model to the output directory
+    """
+    logger.info('Saving model to %s...'%(args.output_dir))
+    model_to_save = model.bert_model.module if hasattr(model.bert_model, 'module') else model.bert_model
+    model_to_save.save_pretrained(args.output_dir)
+    model.tokenizer.save_pretrained(args.output_dir)
+
+def output_ner_predictions(model, batches, dataset, output_file):
+    """
+    Save the prediction as a json file
+    """
+    ner_result = {}
+    span_hidden_table = {}
+    tot_pred_ett = 0
+    for i in range(len(batches)):
+        output_dict = model.run_batch(batches[i], training=False)
+        pred_ner = output_dict['pred_ner']
+        for sample, preds in zip(batches[i], pred_ner):
+            off = sample['sent_start_in_doc'] - sample['sent_start']
+            k = sample['doc_key'] + '-' + str(sample['sentence_ix'])
+            ner_result[k] = []
+            for span, pred in zip(sample['spans'], preds):
+                span_id = '%s::%d::(%d,%d)'%(sample['doc_key'], sample['sentence_ix'], span[0]+off, span[1]+off)
+                if pred == 0:
+                    continue
+                ner_result[k].append([span[0]+off, span[1]+off, ner_id2label[pred]])
+            tot_pred_ett += len(ner_result[k])
+
+    logger.info('Total pred entities: %d'%tot_pred_ett)
+
+    js = dataset.js
+    for i, doc in enumerate(js):
+        doc["predicted_ner"] = []
+        doc["predicted_relations"] = []
+        for j in range(len(doc["sentences"])):
+            k = doc['doc_key'] + '-' + str(j)
+            if k in ner_result:
+                doc["predicted_ner"].append(ner_result[k])
+            else:
+                logger.info('%s not in NER results!'%k)
+                doc["predicted_ner"].append([])
+            
+            doc["predicted_relations"].append([])
+
+        js[i] = doc
+
+    logger.info('Output predictions to %s..'%(output_file))
+    with open(output_file, 'w') as f:
+        f.write('\n'.join(json.dumps(doc, cls=NpEncoder) for doc in js))
